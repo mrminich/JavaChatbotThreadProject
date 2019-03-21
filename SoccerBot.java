7b16bf1adb520278b210391f5bb8caf4299a96eb
Mr. Minich <cminich@wyoarea.org>
2019-03-21 09:51:15 -0400

    helping sumaes
    dont know what to say but teacher taught me its important and i don't use proper grammer cuz my name is repooc

---
+++ SoccerBot.java
@@ -0,0 +1,35 @@
+public class SoccerBot extends Chatbot
+{
+  // constructors
+  public SoccerBot
+  {
+    myBigKick = "";
+    // = 0
+    // = false;
+  }
+
+  // accessor methods (get)
+  public String getBigKick()
+  {
+    return myBigKick;
+  }
+
+  // more here
+
+  // modifier methods (set)
+  public void setBigKick(String bigKick)
+  {
+    myBigKick = bigKick;
+  }
+
+  // more 
+
+  // interesting methods 
+
+  // property (instance variables) declarations
+  private String myBigKick;
+  // int 99
+  // double  3.14
+  // boolean true/false
+
+}
